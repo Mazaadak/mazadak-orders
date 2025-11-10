5a398f443206e2b359c047c52b79e034c907d2f3
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-11-10 16:39:38 +0200

    feat: add and use exceptions from common-module

--- src/main/java/com/mazadak/orders/OrdersApplication.java
+++ src/main/java/com/mazadak/orders/OrdersApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.mazadak.orders", "com.mazadak.common"})
 public class OrdersApplication {
 
     public static void main(String[] args) {
